f65478417a7e90481d40b83e469ad7e49bf297f6
Ihor Palii <paliiihor69@gmail.com>
2024-06-25 11:48:58 +0300

    year=today.year+1

--- 3modul 6_6_my_prog_from_autocheck_go_it.py
+++ 3modul 6_6_my_prog_from_autocheck_go_it.py
@@ -42,11 +42,10 @@ def adjust_for_weekend(birthday):
 def get_upcoming_birthdays(users, days=7):
     upcoming_birthdays = []
     today = date.today()
-    next_year = datetime(year=2025, month=12, day=30).year
     for user in users:
         birthday_this_year = user["birthday"].replace(year=today.year)
         if birthday_this_year < today:
-            birthday_this_year = user["birthday"].replace(year=next_year)
+            birthday_this_year = user["birthday"].replace(year=today.year+1)
 
         if 0 <= (birthday_this_year - today).days <= days:
             birthday_this_year = adjust_for_weekend(birthday_this_year)
